9cefa05bd86283f66582c229c68319ce9b3f16a6
Tom Gobich <fallofmars@gmail.com>
2016-11-07 16:50:50 -0500

    initial commit

---
+++ js/factories/FProducts.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function () {
+
+	'use strict';
+
+	angular.module('app').factory('FProducts', function (FLocalStorage) {
+		// ------------------------------------------------------------
+		// Name: functionName
+		// Abstract: About the functions purpose
+		// ------------------------------------------------------------
+		var functionName = function functionName() {};
+
+		return {};
+	});
+})();
